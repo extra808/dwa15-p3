cac359c7637f21aed91613c5ffe1c6b306c5836e
Curtis Wilcox <extra808@m.cognize.org>
2015-10-12 18:24:43 -0400

    add switch for selecting content format

--- app/Http/Controllers/LoremController.php
+++ app/Http/Controllers/LoremController.php
@@ -15,10 +15,28 @@ class LoremController extends Controller
     * Responds to requests to GET /lorem
     */
     public function getLorem() {
+        $format = '';
         $faker = Faker::create();
         $siteTitle = ' | Developer\'s Best Friend';
         $title = 'Lorem';
-        $content = json_encode($faker->paragraphs(3) );
+        $content = '';
+        $paragraphs = $faker->paragraphs(3);
+        switch ($format) {
+            case 'html' :
+                foreach ($paragraphs as $paragraph) {
+                    $content .= "<p>$paragraph</p>\n";
+                }
+                break;
+            case 'json' :
+                $content = json_encode($paragraphs);
+                break;
+            default :
+                foreach ($paragraphs as $paragraph) {
+                    $content .= "$paragraph\n\n";
+                }
+ 
+        }
+        
         return view('lorem')-> withTitle($title)-> withContent($content)-> withSitetitle($siteTitle);
     }
 }
