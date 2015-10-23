48dfb70caec120fe5c1ed09ab09b400c200e6831
Curtis Wilcox <extra808@m.cognize.org>
2015-10-23 06:11:12 -0400

    add site description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-<p>Site Description
+<p>Developers sometimes need made-up content with which to test their designs. Blocks of text, sometimes used when designing web pages, is often called "Lorem ipsum," after the first two words in commonly used random text taken from an ancient Latin text. This site can generate different kinds of content and in different code formats, ready to copy and paste into your code.
 </p>
 <p><a href="/lorem">Generate random text</a>
 </p>
-<p><a href="/fakeusers">Generate fake users</a>
+<p>Developers often also need made-up user data to test with their code. This site can generate randomly generated names with a variety of additional optional content and in different code formats, ready to copy and paste into your code.
+</p><p><a href="/fakeusers">Generate fake users</a>
 </p>
 @endsection
